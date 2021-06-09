f2d431cd9d3a2723897c4dec9d6e0fa004330aac
kimiimac <kimi0230@gmail.com>
2021-06-09 22:00:09 +0800

    feat: heroku static

--- gogopowerkimibot/settings.py
+++ gogopowerkimibot/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
