438fe1a35aee95d15ac22ca905354df917ba1429
kimiimac <kimi0230@gmail.com>
2021-06-10 21:35:30 +0800

    test

--- app/views.py
+++ app/views.py
@@ -33,6 +33,8 @@ def callback(request):
                     mtext = event.message.text
                     if mtext == '卡比請客':
                         msgresponse.sendText(event, "謝謝卡比 讚嘆卡比 卡比讚讚讚")
+                    elif "幹 bug" in mtext:
+                        msgresponse.sendText(event, "請支援收銀~")
                     elif "吱吱" in mtext:
                         msgresponse.sendImage(event, "zhizhi")
                     elif mtext == "笑鼠人":
