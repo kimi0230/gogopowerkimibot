fad351180c49322a65a19f71e71ce906bf7defa9
kimiimac <kimi0230@gmail.com>
2021-06-16 21:50:30 +0800

    feat: cron

--- services/linenotifyservice.py
+++ services/linenotifyservice.py
@@ -12,11 +12,10 @@ carbeToken = config('CARBE_NOTIFY_TOKEN')
 
 
 def test():
-    print("kimimimimimimimimii")
     msg = '起床尿尿摟'
     payload = {'message': msg}
     headers = {
-        "Authorization": "Bearer " + "msgG1hovWso1zkinUPL0335RVNV1h5TTzET9F1W92Q6",
+        "Authorization": "Bearer " + token,
         "Content-Type": "application/x-www-form-urlencoded"
     }
     notify = requests.post(
@@ -42,5 +41,26 @@ def carbe():
         print('發送 LINE Notify 失敗！')
 
 
+THREE_TRADE = "https://www.twse.com.tw/zh/page/trading/fund/BFI82U.html"
+EGIHTGOV = "https://histock.tw/stock/broker8.aspx"
+ETF = "https://mis.twse.com.tw/stock/etf_nav.jsp?ex=tse#memo3"
+
+
+def stock5pm():
+    msg = '三大法人買賣超: %s \n\n 八大官股: %s \n\n ETF溢價: %s' % (
+        THREE_TRADE, EGIHTGOV, ETF)
+    payload = {'message': msg}
+    headers = {
+        "Authorization": "Bearer " + token,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    notify = requests.post(
+        "https://notify-api.line.me/api/notify", headers=headers, params=payload)
+    if notify.status_code == 200:
+        print('發送 LINE Notify 成功！')
+    else:
+        print('發送 LINE Notify 失敗！')
+
+
 if __name__ == "__main__":
-    test()
+    stock5pm()
