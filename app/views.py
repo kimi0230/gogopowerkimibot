564c95306aaa602f55554ab527efdc1ab207d73e
kimi <kimi0230@gmail.com>
2021-06-24 10:02:06 +0800

    fix: ptt covid19 uirl

--- app/views.py
+++ app/views.py
@@ -52,7 +52,7 @@ def callback(request):
                             resMsg = "%s\n%s\n%s\n" % (
                                 pttRes["Date"], pttRes["Title"], pttRes["Link"])
                         res = covid19service.getCovid19()
-                        resMsg += "%s\n 新增確診:\t %s\n 新增死亡:\t %s\n 累計確診:\t %s\n 累計死亡:\t %s\n 死亡率:\t %s\n 疫苗接種人次:\t %s\n %s" % (
+                        resMsg += "\n%s\n 新增確診:\t %s\n 新增死亡:\t %s\n 累計確診:\t %s\n 累計死亡:\t %s\n 死亡率:\t %s\n 疫苗接種人次:\t %s\n %s" % (
                             res["time"], res["recovered"], res["newDeaths"], res["total"], res["totalDeaths"], res["rateDeaths"], res["vaccine"], res["url"])
                         if resMsg != "":
                             msgresponse.sendText(event, resMsg)
