2d0d9a7a8d4bc72f0d3aabcab0b0a6cbc6b70d42
kimi <kimi0230@gmail.com>
2021-06-25 18:28:15 +0800

    feat: translate

--- services/cambridgeservice.py
+++ services/cambridgeservice.py
@@ -14,67 +14,81 @@ headers = {
 
 
 def transWord(word):
-    wordURL = 'https://dictionary.cambridge.org/zht/%E8%A9%9E%E5%85%B8/%E8%8B%B1%E8%AA%9E-%E6%BC%A2%E8%AA%9E-%E7%B9%81%E9%AB%94/'+word
-    res = requests.get(wordURL, headers=headers)
-    soup = BeautifulSoup(res.text, "lxml")
-    result = {}
+    try:
+        wordURL = 'https://dictionary.cambridge.org/zht/%E8%A9%9E%E5%85%B8/%E8%8B%B1%E8%AA%9E-%E6%BC%A2%E8%AA%9E-%E7%B9%81%E9%AB%94/'+word
+        res = requests.get(wordURL, headers=headers)
+        soup = BeautifulSoup(res.text, "lxml")
+        result = {}
 
-    result["Head"] = []
-    # 詞性, 發音
-    for head in soup.select(".pos-header.dpos-h"):
-        # 詞性
-        partofspeech = head.select('.pos.dpos')[0].text
-        # kk 音標
-        kk = head.select('.ipa.dipa.lpr-2.lpl-1')
-        uk = "[ %s ]" % (kk[0].text)
-        ukAudio = url + \
-            head.select('source')[0].get("src")
-        us = "[ %s ]" % (kk[1].text)
-        usAudio = url + \
-            head.select('source')[2].get("src")
-        result["Head"].append(
-            {"uk": uk, "ukAudio": ukAudio, "us": us, "usAudio": usAudio, "partofspeech": partofspeech})
+        result["Head"] = []
+        # 詞性, 發音
+        for head in soup.select(".pos-header.dpos-h"):
+            # 詞性
+            partofspeech = head.select('.pos.dpos')[0].text
+            # kk 音標
+            kk = head.select('.ipa.dipa.lpr-2.lpl-1')
+            uk = "[ %s ]" % (kk[0].text)
+            ukAudio = url + \
+                head.select('source')[0].get("src")
+            us = "[ %s ]" % (kk[1].text)
+            usAudio = url + \
+                head.select('source')[2].get("src")
+            result["Head"].append(
+                {"uk": uk, "ukAudio": ukAudio, "us": us, "usAudio": usAudio, "partofspeech": partofspeech})
+        if len(result["Head"]) <= 0:
+            return ""
 
-    # 中文意思
-    chtTranslation = set(soup.select(".trans.dtrans.dtrans-se.break-cj")) - \
-        set(soup.select(".trans.dtrans.dtrans-se.hdb.break-cj"))
-    result["ChtTrans"] = [item.get_text().strip() for item in chtTranslation]
+        # 中文意思
+        chtTranslation = set(soup.select(".trans.dtrans.dtrans-se.break-cj")) - \
+            set(soup.select(".trans.dtrans.dtrans-se.hdb.break-cj"))
+        result["ChtTrans"] = [item.get_text().strip()
+                              for item in chtTranslation]
 
-    # 英文意思
-    engTranslation = soup.select(".ddef_h")
-    result["EngTrans"] = [item.get_text().strip() for item in engTranslation]
+        # 英文意思
+        engTranslation = soup.select(".ddef_h")
+        result["EngTrans"] = [item.get_text().strip()
+                              for item in engTranslation]
 
-    # 例子
-    example = soup.select(".examp.dexamp")
-    result["Example"] = [item.get_text().strip() for item in example]
+        # 例子
+        example = soup.select(".examp.dexamp")
+        result["Example"] = [item.get_text().strip() for item in example]
 
-    return result
+        return result
+    except:
+        return ""
 
 
 def toMsg(source, examp=True):
-    if source != "":
-        result = "\n"
-        for head in source["Head"]:
-            result += "%s\n UK : %s US: %s \n" % (
-                head["partofspeech"], head["uk"], head["us"])
-        result += "---------\n"
-        for k, v in enumerate(source["ChtTrans"]):
-            result += "%s \n %s  \n" % (v, source["EngTrans"][k])
-        if examp == True:
-            result += "\n---------\n"
-            result += "\nExample:\n "+"\n\n".join(source["Example"])
-    else:
-        return
-    return result
+    try:
+        if source != "":
+            result = "\n"
+            for head in source["Head"]:
+                result += "%s\n UK : %s US: %s \n" % (
+                    head["partofspeech"], head["uk"], head["us"])
+            result += "---------\n"
+            for k, v in enumerate(source["ChtTrans"]):
+                result += "%s \n %s  \n" % (v, source["EngTrans"][k])
+            if examp == True:
+                result += "\n---------\n"
+                result += "\nExample:\n "+"\n\n".join(source["Example"])
+        else:
+            return ""
+        return result
+    except:
+        return ""
 
 
 def getDailyAWord():
-    res = requests.get(url, headers=headers)
-    res.encoding = 'UTF-8'
-    soup = BeautifulSoup(res.text, "lxml")
-    word = soup.select(".fs36.lmt-5.feature-w-big.wotd-hw a")[0].text
-    return transWord(word)
+    try:
+        res = requests.get(url, headers=headers)
+        res.encoding = 'UTF-8'
+        soup = BeautifulSoup(res.text, "lxml")
+        word = soup.select(".fs36.lmt-5.feature-w-big.wotd-hw a")[0].text
+        return transWord(word)
+    except:
+        return ""
 
 
 if __name__ == "__main__":
-    print(getDailyAWord())
+    # print(getDailyAWord())
+    print(transWord("kioki"))
