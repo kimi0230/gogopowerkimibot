7c7848b1f4603d75a7b172f432375fc1abcfad09
kimiimac <kimi0230@gmail.com>
2021-06-25 19:34:45 +0800

    test

--- services/cambridgeservice.py
+++ services/cambridgeservice.py
@@ -15,6 +15,9 @@ headers = {
 
 def transWord(word):
     try:
+        if word == "":
+            return ""
+        word = word.strip()
         wordURL = 'https://dictionary.cambridge.org/zht/%E8%A9%9E%E5%85%B8/%E8%8B%B1%E8%AA%9E-%E6%BC%A2%E8%AA%9E-%E7%B9%81%E9%AB%94/'+word
         res = requests.get(wordURL, headers=headers)
         soup = BeautifulSoup(res.text, "lxml")
