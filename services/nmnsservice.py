64545ff998da453018a81ab3426c039353ff3667
kimiimac <kimi0230@gmail.com>
2021-09-11 16:01:31 +0800

    feat: 天文

--- services/nmnsservice.py
+++ services/nmnsservice.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 import datetime
+# from datetime import datetime
+from utility import tinyURL
+import re
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36'
@@ -25,18 +28,23 @@ def getStar(year="", month=""):
         contents = [] if len(soup.select('.ts_overflow_Xh p')
                              ) == 0 else [p.text.strip() for p in soup.select('.ts_overflow_Xh p') if p.text.strip() != ""]
 
-        contentTitle = soup.select('#content-title h2')[0].text.strip()
+        reg = r"[\u4e00-\u9fa5]{1,2}月[\u4e00 -\u9fa5]{1,2}日"
+        contentsTitle = [] if len(soup.select('.bold-title')
+                                  ) == 0 else [transZhTime2Arabic(p.text.strip(), reg) for p in soup.select('.bold-title') if p.text.strip() != ""]
+
+        title = soup.select('#content-title h2')[0].text.strip()
 
         images = []if len(soup.select('.ts_overflow_Xh a')
                           ) == 0 else [{"title": a.get("title"), "link": a.get("href")} for a in soup.select('.ts_overflow_Xh a') if a.get("title") != None]
 
         result = {
             "url": url,
-            "contentTitle": contentTitle,
+            "title": title,
             "contents": contents[:-2],  # 後面的聯絡資訊不要
+            "contentsTitle": contentsTitle,
             "images": images
         }
-        # print(result)
+
         return result
     except Exception as e:
         print(e)
@@ -45,15 +53,41 @@ def getStar(year="", month=""):
 
 def getStarText(year="", month=""):
     star = getStar(year, month)
-    msg = star['contentTitle']+"\t"+star['url'] + \
+    msg = star['title']+"\t"+star['url'] + \
         "\n"+star['contents'][0] + "\n\n"
 
-    msg += "\n".join(star['contents'][1:])
-    # print(msg)
+    msg += "\n".join(s['title'] for s in star['contentsTitle'])
+    print(msg)
     return msg
+
+
+def transZhTime2Arabic(msg, reg):
+    # year = str.split('年')[0]
+    dateStr = re.search(reg, msg).group()
+    month = dateStr.split('月')[0]
+    day = dateStr.split('月')[1].split('日')[0]
+
+    NUM = {'零': '0', '一': '1', '二': '2',
+           '三': '3', '四': '4', '五': '5', '六': '6', '七': '7', '八': '8', '九': '9', '十': '10'}
+
+    if len(day) > 2:
+        day = day[0]+day[1]
+
+    month = ''.join(str(NUM[i]) for i in month)
+    day = ''.join(str(NUM[i]) for i in day)
+
+    if len(month) == 3:
+        month = '1'+month[2]
+    if len(day) == 3:
+        day = '1'+day[2]
+    return{"title": msg.replace(dateStr, month+"/"+day), "day": month+"/"+day}
 
 
 if __name__ == "__main__":
     # python3 -B -m services.nmnsservice
     # getStar()
     getStarText()
+    # transZhTime2Arabic("二、九月九日 水星合月", r"[\u4e00-\u9fa5]+月[\u4e00-\u9fa5]日+")
+    # a, b = transZhTime2Arabic(
+    #     "二、九月一九日 水星合月", r"[\u4e00-\u9fa5]{1,2}月[\u4e00 -\u9fa5]{1,2}日")
+    # print(b)
