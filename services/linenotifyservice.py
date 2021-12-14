a93e5e944c908591cfc96b4078e4b88e3e5cce64
kimi <kimi0230@gmail.com>
2021-12-14 11:08:53 +0800

    test

--- services/linenotifyservice.py
+++ services/linenotifyservice.py
@@ -676,7 +676,7 @@ if __name__ == "__main__":
     # getPresume()
     # ivy(3)
     checkinBooks([{
-        "cookies": {"Cookie": "bid=618b235c684f2; item_history=F014084384+; ssid=618b235c684f2.1639372812; bt=r41g5o; lpk=dc2d5afdeb0b093dbd52498395398104b438e1c4e7b64a6081a271edb8ded5ff5e414c3499410886; cid=sherlock8; bday=1989/02/01; pd=B4sJqHxLBW3.UO9EP7BnKgErGY; gud=4780f2b43e12a7e68c202bb28a4259f9712d86348704730e6590213d7d5a27290477d4193ab2025720eb817159d71662f7bc1473a6cf7fc677c788f9b1703749; bid=61b6d813cc5e7"},
+        "cookies": {"Cookie": ""},
         "tokenStr": "Kimi"
     }])
     # checkinShopee([{
