a4897c48167ffe94b70388e810d9ad8b29dc301f
kimiimac <kimi0230@gmail.com>
2022-01-09 23:20:29 +0800

    kimi0230

--- services/linenotifyservice.py
+++ services/linenotifyservice.py
@@ -649,7 +649,7 @@ def getKimi0230():
     if msg == None:
         return
     payload = {
-        'message': "https://visitor-badge.glitch.me/badge?page_id=kimi0230 : " + msg}
+        'message': "https://github.com/kimi0230 : " + msg}
     res = sendLineNotify(token, payload)
 
     if res.status_code == 200:
