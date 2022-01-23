e752c818f61d9ee6f42bbc98b6914cdbe829c59f
kimiimac <kimi0230@gmail.com>
2022-01-23 14:34:46 +0800

    covid19

--- app/views.py
+++ app/views.py
@@ -52,7 +52,12 @@ def callback(request):
                         if len(pttRes) > 0:
                             resMsg = "%s\n%s\n%s\n" % (
                                 pttRes["Date"], pttRes["Title"], pttRes["Link"])
-                        res = covid19service.getCovid19()
+                        officalRes = covid19service.getCovid19()
+                        if officalRes != "":
+                            nowDateEng = datetime.now().strftime("%b-%-d")
+                            regex = re.compile(r'^Updated on '+nowDateEng+'.*')
+                            if regex.match(officalRes["time"]) == None:
+                                resMsg += "\n你家的政府官網沒更新!\n"
                         resMsg += "\n%s\n 新增確診:\t %s (本土 %s, 境外 %s) \n 新增死亡:\t %s\n 累計確診:\t %s\n 累計死亡:\t %s\n 死亡率:\t %s\n 疫苗接種人次:\t %s %s\n %s" % (
                             res["time"], res["recovered"], res["domesticRecovered"], res["internationalRecovered"], res["newDeaths"], res["total"], res["totalDeaths"], res["rateDeaths"], res["vaccine"], res["vaccinePercent"], res["url"])
                         if resMsg != "":
