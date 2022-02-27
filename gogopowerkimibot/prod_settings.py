c3c44af33b9babc4c58006cc629de2bf6fda67af
kimiimac <kimi0230@gmail.com>
2022-02-27 21:27:44 +0800

    test

--- gogopowerkimibot/prod_settings.py
+++ gogopowerkimibot/prod_settings.py
@@ -2,5 +2,4 @@ from .settings import *
 STATIC_ROOT = 'staticfiles'
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # ALLOWD_HOSTS = ['gogopowerkimibot.herokuapp.com']
-ALLOWD_HOSTS = ['*']
 DEBUG = False
