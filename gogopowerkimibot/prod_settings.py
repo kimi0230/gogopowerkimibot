ec315ee4c1c97f956cf73416ace837a8c915b7e7
kimiimac <kimi0230@gmail.com>
2022-02-27 21:23:12 +0800

    disable debug

--- gogopowerkimibot/prod_settings.py
+++ gogopowerkimibot/prod_settings.py
@@ -3,4 +3,4 @@ STATIC_ROOT = 'staticfiles'
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # ALLOWD_HOSTS = ['gogopowerkimibot.herokuapp.com']
 ALLOWD_HOSTS = ['*']
-DEBUG = True
+DEBUG = False
