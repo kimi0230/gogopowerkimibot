57f9a1e98e56a2e886155a7fb8b8d4a067e2fa60
kimiimac <kimi0230@gmail.com>
2022-02-28 10:21:05 +0800

    wakeup

--- wakeup.py
+++ wakeup.py
@@ -1,10 +1,12 @@
 import urllib.request
+import datetime
+from decouple import config
 
 
 def wakeup():
-    url = "https://xxx.com"
+    url = config('APP_URL', default='APP_URL')
     conn = urllib.request.urlopen(url)
-
+    print("wakeup at : ", datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S"))
     for key, value in conn.getheaders():
         print(key, value)
 
