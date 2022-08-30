4bf3ed2a71688ca5e28cc7edb2446a3f4414c2a4
kimi <kimi0230@gmail.com>
2022-08-30 10:05:46 +0800

    fix: config

--- services/mailservice.py
+++ services/mailservice.py
@@ -1,12 +1,24 @@
 import smtplib
 import os
 from django.conf import settings
+from decouple import config
 
-smtpObj = smtplib.SMTP(settings.GMAIL_SMTP,
-                       settings.GMAIL_TLS_PORT)
+
+try:
+    GMAIL_SMTP = settings.GMAIL_SMTP
+    GMAIL_TLS_PORT = settings.GMAIL_TLS_PORT
+    GMAIL_USER = settings.GMAIL_USER
+    GMAIL_PASSWORD = settings.GMAIL_USER
+except:
+    GMAIL_SMTP = config('GMAIL_SMTP')
+    GMAIL_TLS_PORT = config('GMAIL_TLS_PORT')
+    GMAIL_USER = config('GMAIL_USER')
+    GMAIL_PASSWORD = config('GMAIL_PASSWORD')
+
+smtpObj = smtplib.SMTP(GMAIL_SMTP, GMAIL_TLS_PORT)
 smtpObj.ehlo()
 smtpObj.starttls()
-smtpObj.login(settings.GMAIL_USER, settings.GMAIL_PASSWORD)
+smtpObj.login(GMAIL_USER, GMAIL_PASSWORD)
 
 
 def HelloWord():
@@ -16,8 +28,7 @@ def HelloWord():
 
 def SendMail(to, subject, body):
     mailBody = "Subject: %s\n%s" % (subject, body.as_string())
-    # print("mailBody --->", config('GMAIL_USER'), mailBody)
-    smtpObj.sendmail(settings.GMAIL_USER, to,
+    smtpObj.sendmail(GMAIL_USER, to,
                      mailBody)
 
 
